c01ddac998560d290b4c012c1c02aa45cd67d817
Alan Hynes <g00400498@atu.ie>
2025-01-02 17:20:01 +0000

    created and populated payment entity

---
+++ src/main/java/ie/atu/paymentservice/Payment.java
@@ -0,0 +1,23 @@
+package ie.atu.paymentservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long rideId;
+    private Double amount;
+    private String status;
+}
