4e1f241c61f865a1c7742bfe882c40fb6301faa0
G00400498@atu.ie <g00400498@atu.ie>
2025-01-08 15:07:07 +0000

    amended payment repository

--- src/main/java/ie/atu/paymentservice/PaymentRepository.java
+++ src/main/java/ie/atu/paymentservice/PaymentRepository.java
@@ -3,6 +3,10 @@ package ie.atu.paymentservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByRideId(Long rideId);
+    List<Payment> findByStatus(String status);
 }
